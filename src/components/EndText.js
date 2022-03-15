5f3b0ae626f468c28ddd16a319ca0a1c8a6d66c5
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2022-03-15 02:52:17 +0100

    * improve + added more end text

--- src/components/EndText.js
+++ src/components/EndText.js
@@ -3,6 +3,7 @@ import {Card, Col, Container, ListGroup, ListGroupItem, Row} from "react-bootstr
 import pipelineImage from "../img/pipeline.png";
 import compartmentImage from "../img/compartments.png"
 import fullPipelineImage from "../img/full-pipeline.png"
+import resultsImage from "../img/results.png"
 
 function coverWith(image) {
     return {
@@ -15,46 +16,68 @@ export class EndText extends Component {
     render() {
         return (<Container className={"mt-3"}>
             <Row xs={1} md={2} className="g-4">
-                <Card style={coverWith(pipelineImage)} className={"p-3"}>
-                    <Row>
-                        <Col>
-                        </Col>
-                        <Col className={"bg-blur text-white rounded-2 "}>
-                            <Card.Body>
-                                <Card.Title>Nicht so einfach ...</Card.Title>
-                                <Card.Text >Natürlich ist das eigentliche Programm nicht so einfach. Aber es ist nicht in Computercode geschrieben, sondern
-                                mit einer <i>visuellen Programmiersprache</i>. Das heißt, man baut einzelne Schritte in einer grafischen Umgebung zusammen und muss nicht
-                                zusätlich programmieren lernen.</Card.Text>
-                            </Card.Body>
-                        </Col>
-                    </Row>
-                </Card>
-                <Card style={coverWith(compartmentImage)} className={"p-3"}>
-                    <Row>
-                        <Col>
-                        </Col>
-                        <Col className={"bg-blur text-white rounded-2 "}>
-                            <Card.Body>
-                                <Card.Title>Schritt für Schritt</Card.Title>
-                                <Card.Text >In dem Programm "JIPipe" wurde eine vollständig automatische Analyse für die Wurmdaten gebaut. Eine vereinfachte Version hast
-                                du hier auf der Seite gesehen. In Echt ist das Finden der richtigen Reihenfolge aber etwas schwieriger. Zum Beispiel hat JIPipe alleine
-                                fast 1000 Funktionen zur Verfügung.<br/>Schau dir doch mal die <a href={fullPipelineImage} target={"_blank"}>vollständige</a> Pipeline an (Achtung: Großes Bild!)</Card.Text>
-                            </Card.Body>
-                        </Col>
-                    </Row>
-                </Card>
-                <Card>
-                    <Card.Body>
-                        <Card.Title>Quellen</Card.Title>
-                        <Card.Text>
-                            Hier findest du weiterführende Informationen, falls dich das Thema interessiert:
-                        </Card.Text>
-                    </Card.Body>
-                    <ListGroup className={"list-group-flush"}>
-                        <ListGroupItem>JIPipe Webseite (Englisch): <a href={"https://www.jipipe.org"} target={"_blank"}>https://www.jipipe.org</a></ListGroupItem>
-                        <ListGroupItem>Wurm-Analyse: Veröffentlichung in einem wissenschaftlichen Journal (Englisch): <a href={"https://doi.org/10.1073/pnas.2110669118"} target={"_blank"}>Büttner H, Niehs SP, Vandelannoote K, Cseresnyés Z, Dose B, Richter I, Gerst R, Figge MT, Stinear TP, Pidot SJ, Hertweck C (2021) Bacterial endosymbionts protect beneficial soil fungus from nematode attack. Proc Natl Acad Sci U S A, 118 (37) e2110669118</a></ListGroupItem>
-                    </ListGroup>
-                </Card>
+                <Col>
+                    <Card style={coverWith(pipelineImage)} className={"p-3 h-100"}>
+                        <Row>
+                            <Col>
+                            </Col>
+                            <Col className={"bg-blur text-white rounded-2 "}>
+                                <Card.Body>
+                                    <Card.Title>Nicht so einfach ...</Card.Title>
+                                    <Card.Text >Natürlich ist das eigentliche Programm nicht so einfach. Aber es ist nicht in Computercode geschrieben, sondern
+                                        mit einer <i>visuellen Programmiersprache</i>. Das heißt, man baut einzelne Schritte in einer grafischen Umgebung zusammen und muss nicht
+                                        zusätzlich programmieren lernen.</Card.Text>
+                                </Card.Body>
+                            </Col>
+                        </Row>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card style={coverWith(compartmentImage)} className={"p-3 h-100"}>
+                        <Row>
+                            <Col>
+                            </Col>
+                            <Col className={"bg-blur text-white rounded-2 "}>
+                                <Card.Body>
+                                    <Card.Title>Schritt für Schritt</Card.Title>
+                                    <Card.Text >In dem Programm "JIPipe" wurde eine vollständig automatische Analyse für die Wurmdaten gebaut. Eine vereinfachte Version hast
+                                        du hier auf der Seite gesehen. In Echt ist das Finden der richtigen Reihenfolge aber etwas schwieriger. Zum Beispiel hat JIPipe alleine
+                                        fast 1000 Funktionen zur Verfügung.<br/>Schau dir doch mal die <a href={fullPipelineImage} target={"_blank"}>vollständige</a> Pipeline an (Achtung: Großes Bild!)</Card.Text>
+                                </Card.Body>
+                            </Col>
+                        </Row>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card style={coverWith(resultsImage)} className={"p-3 h-100"}>
+                        <Row>
+                            <Col>
+                            </Col>
+                            <Col className={"bg-blur text-white rounded-2 "}>
+                                <Card.Body>
+                                    <Card.Title>Viel zu tun!</Card.Title>
+                                    <Card.Text >Mikroskope und andere Verfahren werden immer besser. Zum Beispiel kann man mit modernen Verfahren sogar einzelne Moleküle sehen!
+                                    Wie muss man Programme anpassen, damit sie damit umgehen können? Wie kann man in Echtzeit Analysen ausführen und den Computer helfen lassen?
+                                    Wie sollen Ärzte die Programme bedienen können? Schließlich benötigt man starke Computer dafür. Wie kann man Zusammenarbeit verbessern? Und das sind nur einige Punkte wo frische Ideen helfen können!</Card.Text>
+                                </Card.Body>
+                            </Col>
+                        </Row>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card className={"p-3 h-100"}>
+                        <Card.Body>
+                            <Card.Title>Quellen</Card.Title>
+                            <Card.Text>
+                                Hier findest du weiterführende Informationen, falls dich das Thema interessiert:
+                            </Card.Text>
+                        </Card.Body>
+                        <ListGroup className={"list-group-flush"}>
+                            <ListGroupItem>JIPipe Webseite (Englisch): <a href={"https://www.jipipe.org"} target={"_blank"}>https://www.jipipe.org</a></ListGroupItem>
+                            <ListGroupItem>Wurm-Analyse: Veröffentlichung in einem wissenschaftlichen Journal (Englisch): <a href={"https://doi.org/10.1073/pnas.2110669118"} target={"_blank"}>Büttner H, Niehs SP, Vandelannoote K, Cseresnyés Z, Dose B, Richter I, Gerst R, Figge MT, Stinear TP, Pidot SJ, Hertweck C (2021) Bacterial endosymbionts protect beneficial soil fungus from nematode attack. Proc Natl Acad Sci U S A, 118 (37) e2110669118</a></ListGroupItem>
+                        </ListGroup>
+                    </Card>
+                </Col>
             </Row>
         </Container>)
     }
--- src/components/InfoText.js
+++ src/components/InfoText.js
@@ -17,91 +17,103 @@ export class InfoText extends Component {
     render() {
         return (<Container className={"mt-3"}>
             <Row xs={1} md={2} className="g-4">
-                <Card style={coverWith(hyphaeImage)}>
-                    <Row className={"p-3"}>
-                        <Col>
-                        </Col>
-                        <Col className={"bg-blur text-white rounded-2"}>
-                            <Card.Body>
-                                <Card.Title>Im Boden lebende Pilze sind gut für das Pflanzenwachstum</Card.Title>
-                                <Card.Text >Auf dem Bild siehst du eine Mikroskopieaufnahme eines Pilzfadens (Hyphe).
-                                    Diese ist mit einem Farbstoff blau gefärbt.
-                                    Innen drin sieht man grün gefärbe Bakterien, die mit dem Pilz in einer Symbiose (beide haben etwas davon) zusammenleben.</Card.Text>
-                            </Card.Body>
-                        </Col>
-                    </Row>
-                </Card>
-                <Card style={coverWith(nematodeImage)} className={"p-3"}>
-                    <Row>
-                        <Col>
-                        </Col>
-                        <Col className={"bg-blur text-white rounded-2 "}>
-                            <Card.Body>
-                                <Card.Title>Fadenwürmer sind natürliche Fressfeinde der Pilze</Card.Title>
-                                <Card.Text >Auf dem Bild siehst du eine Mikroskopieaufnahme des Pilzes und einiger Fadenwürmer (Nematoden), die den Pilz fressen.
-                                    <strong> Der Pilz ist aber nicht komplett wehrlos!</strong></Card.Text>
-                            </Card.Body>
-                        </Col>
-                    </Row>
-                </Card>
-                <Card style={coverWith(teamImage)} className={"p-3"}>
-                    <Row>
-                        <Col>
-                        </Col>
-                        <Col className={"bg-blur text-white rounded-2 "}>
-                            <Card.Body>
-                                <Card.Title>Keine Ahnung? Forschen!</Card.Title>
-                                <Card.Text>Zu verstehen wie Pilze sich wehren, kann z.B. zur Entwicklung von umweltfreundlichen und bodenschonenden Alternativen
-                                von Düngung und Giften beitragen. Deshalb haben ein Team von Forschern bestehend aus <i>Hannah Büttner, Sarah P. Niehs, Koen Vandelannoote, Zoltán Cseresnyés,
-                                        Benjamin Dose, Ingrid Richter, Ruman Gerst, Marc Thilo Figge, Timothy P. Stinear, Sacha J. Pidot, und Christian Hertweck</i> sich diese Interaktion etwas näher angeschaut.</Card.Text>
-                            </Card.Body>
-                        </Col>
-                    </Row>
-                </Card>
-                <Card style={coverWith(nematodeAreaImage)} className={"p-3"}>
-                    <Row>
-                        <Col>
-                        </Col>
-                        <Col className={"bg-blur text-white rounded-2 "}>
-                            <Card.Body>
-                                <Card.Title>Rolle der Bakterien</Card.Title>
-                                <Card.Text >Es wurde untersucht, ob nicht vielleicht die Bakterien ein Gift gegen die Würmer produziert.
-                                    Dazu wurden diese z.B. mit Antibiotika abgetötet. Um zu schauen, ob Würmer leben, prüft man einfach wie weit sie sich bewegen.
-                                    <br/> Das will aber keiner von Hand machen! Hunderte Würmer sind auf einem einzigen Mikroskopiebild.
-                                </Card.Text>
-                            </Card.Body>
-                        </Col>
-                    </Row>
-                </Card>
-                <Card style={coverWith(pipelineImage)} className={"p-3"}>
-                    <Row>
-                        <Col>
-                        </Col>
-                        <Col className={"bg-blur text-white rounded-2 "}>
-                            <Card.Body>
-                                <Card.Title>Automatische Analyse mit dem Computer</Card.Title>
-                                <Card.Text >Statt alles per Hand zu zählen, wurde ein Computerprogramm entwickelt, das alle Würmer automatisch findet,
-                                    über die Zeit verfolgt und alles in Tabellen und Diagramme schreibt. <br/>
-                                    Einfach das Programm anwerfen und nach dem Mittagessen sind alle Ergebnisse da.<br/>
-                                    <strong>Ein Programm kann aber nicht deine Gedanken lesen! Es ist viel Aufwand damit verbunden, alles einzustellen.
-                                    Probiere es doch einmal selbst aus!</strong>
-                                </Card.Text>
-                            </Card.Body>
-                        </Col>
-                    </Row>
-                </Card>
-                <Card>
-                    <Card.Body>
-                        <Card.Title>Quellen</Card.Title>
-                        <Card.Text>
-                            Hier findest du weiterführende Informationen, falls dich das Thema interessiert:
-                        </Card.Text>
-                    </Card.Body>
-                    <ListGroup className={"list-group-flush"}>
-                        <ListGroupItem>Vollständiger Text: <a href={"https://www.leibniz-hki.de/de/pressemitteilung/mikroben-vereint-gegen-den-feind.html"} target={"_blank"}>https://www.leibniz-hki.de/de/pressemitteilung/mikroben-vereint-gegen-den-feind.html</a></ListGroupItem>
-                        <ListGroupItem>Veröffentlichung in einem wissenschaftlichen Journal (Englisch): <a href={"https://doi.org/10.1073/pnas.2110669118"} target={"_blank"}>Büttner H, Niehs SP, Vandelannoote K, Cseresnyés Z, Dose B, Richter I, Gerst R, Figge MT, Stinear TP, Pidot SJ, Hertweck C (2021) Bacterial endosymbionts protect beneficial soil fungus from nematode attack. Proc Natl Acad Sci U S A, 118 (37) e2110669118</a></ListGroupItem>
-                    </ListGroup>
-                </Card>
+                <Col>
+                    <Card style={coverWith(hyphaeImage)} className={"p-3 h-100"}>
+                        <Row >
+                            <Col>
+                            </Col>
+                            <Col className={"bg-blur text-white rounded-2"}>
+                                <Card.Body>
+                                    <Card.Title>Im Boden lebende Pilze sind gut für das Pflanzenwachstum</Card.Title>
+                                    <Card.Text >Auf dem Bild siehst du eine Mikroskopieaufnahme eines Pilzfadens (Hyphe).
+                                        Diese ist mit einem Farbstoff blau gefärbt.
+                                        Innen drin sieht man grün gefärbe Bakterien, die mit dem Pilz in einer Symbiose (beide haben etwas davon) zusammenleben.</Card.Text>
+                                </Card.Body>
+                            </Col>
+                        </Row>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card style={coverWith(nematodeImage)} className={"p-3 h-100"}>
+                        <Row>
+                            <Col>
+                            </Col>
+                            <Col className={"bg-blur text-white rounded-2 "}>
+                                <Card.Body>
+                                    <Card.Title>Fadenwürmer sind natürliche Fressfeinde der Pilze</Card.Title>
+                                    <Card.Text >Auf dem Bild siehst du eine Mikroskopieaufnahme des Pilzes und einiger Fadenwürmer (Nematoden), die den Pilz fressen.
+                                        <strong> Der Pilz ist aber nicht komplett wehrlos!</strong></Card.Text>
+                                </Card.Body>
+                            </Col>
+                        </Row>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card style={coverWith(teamImage)} className={"p-3 h-100"}>
+                        <Row>
+                            <Col>
+                            </Col>
+                            <Col className={"bg-blur text-white rounded-2 "}>
+                                <Card.Body>
+                                    <Card.Title>Keine Ahnung? Forschen!</Card.Title>
+                                    <Card.Text>Zu verstehen wie Pilze sich wehren, kann z.B. zur Entwicklung von umweltfreundlichen und bodenschonenden Alternativen
+                                        von Düngung und Giften beitragen. Deshalb haben ein Team von Forschern bestehend aus <i>Hannah Büttner, Sarah P. Niehs, Koen Vandelannoote, Zoltán Cseresnyés,
+                                            Benjamin Dose, Ingrid Richter, Ruman Gerst, Marc Thilo Figge, Timothy P. Stinear, Sacha J. Pidot, und Christian Hertweck</i> sich diese Interaktion etwas näher angeschaut.</Card.Text>
+                                </Card.Body>
+                            </Col>
+                        </Row>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card style={coverWith(nematodeAreaImage)} className={"p-3 h-100"}>
+                        <Row>
+                            <Col>
+                            </Col>
+                            <Col className={"bg-blur text-white rounded-2 "}>
+                                <Card.Body>
+                                    <Card.Title>Rolle der Bakterien</Card.Title>
+                                    <Card.Text >Es wurde untersucht, ob nicht vielleicht die Bakterien ein Gift gegen die Würmer produziert.
+                                        Dazu wurden diese z.B. mit Antibiotika abgetötet. Um zu schauen, ob Würmer leben, prüft man einfach wie weit sie sich bewegen.
+                                        <br/> Das will aber keiner von Hand machen! Hunderte Würmer sind auf einem einzigen Mikroskopiebild.
+                                    </Card.Text>
+                                </Card.Body>
+                            </Col>
+                        </Row>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card style={coverWith(pipelineImage)} className={"p-3 h-100"}>
+                        <Row>
+                            <Col>
+                            </Col>
+                            <Col className={"bg-blur text-white rounded-2 "}>
+                                <Card.Body>
+                                    <Card.Title>Automatische Analyse mit dem Computer</Card.Title>
+                                    <Card.Text >Statt alles per Hand zu zählen, wurde ein Computerprogramm entwickelt, das alle Würmer automatisch findet,
+                                        über die Zeit verfolgt und alles in Tabellen und Diagramme schreibt. <br/>
+                                        Einfach das Programm anwerfen und nach dem Mittagessen sind alle Ergebnisse da.<br/>
+                                        <strong>Ein Programm kann aber nicht deine Gedanken lesen! Es ist viel Aufwand damit verbunden, alles einzustellen.
+                                            Probiere es doch einmal selbst aus!</strong>
+                                    </Card.Text>
+                                </Card.Body>
+                            </Col>
+                        </Row>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card className={"p-3 h-100"}>
+                        <Card.Body>
+                            <Card.Title>Quellen</Card.Title>
+                            <Card.Text>
+                                Hier findest du weiterführende Informationen, falls dich das Thema interessiert:
+                            </Card.Text>
+                        </Card.Body>
+                        <ListGroup className={"list-group-flush"}>
+                            <ListGroupItem>Vollständiger Text: <a href={"https://www.leibniz-hki.de/de/pressemitteilung/mikroben-vereint-gegen-den-feind.html"} target={"_blank"}>https://www.leibniz-hki.de/de/pressemitteilung/mikroben-vereint-gegen-den-feind.html</a></ListGroupItem>
+                            <ListGroupItem>Veröffentlichung in einem wissenschaftlichen Journal (Englisch): <a href={"https://doi.org/10.1073/pnas.2110669118"} target={"_blank"}>Büttner H, Niehs SP, Vandelannoote K, Cseresnyés Z, Dose B, Richter I, Gerst R, Figge MT, Stinear TP, Pidot SJ, Hertweck C (2021) Bacterial endosymbionts protect beneficial soil fungus from nematode attack. Proc Natl Acad Sci U S A, 118 (37) e2110669118</a></ListGroupItem>
+                        </ListGroup>
+                    </Card>
+                </Col>
             </Row>
         </Container>)
     }
